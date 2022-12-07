c1838e21250ebf957c28adc20b899f18482d420a
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2022-12-07 19:22:10 +0500

    Update Program.cs

--- DotNetWebBootcamp/Day5Exercises/Day5/Program.cs
+++ DotNetWebBootcamp/Day5Exercises/Day5/Program.cs
@@ -6,63 +6,69 @@ namespace Day5
     {
         static void Main(string[] args)
         {
-            InsertionSort();
-        }
+            OptimizedBubbleSort();
+            Console.WriteLine("");
 
-        public static void BubbleSort()
-        {
-            int[] arr = { 80, 11, 590, 771, 9, 770, 240, 9 };
-            for (int write = 0; write < arr.Length; write++)
-            {
-                for (int sort = 0; sort < arr.Length - 1; sort++)
-                {
-                    if (arr[sort] > arr[sort + 1])
-                    {
-                        int temp = 0;
-                        temp = arr[sort + 1];
-                        arr[sort + 1] = arr[sort];
-                        arr[sort] = temp;
-                    }
-                }
-            }
-
-            for (int i = 0; i < arr.Length; i++)
-                Console.Write(arr[i] + " ");
+            OptimizedInsertionSort();
+            Console.WriteLine("\n");
 
             Console.ReadKey();
         }
+
+                //Add an extra variable swap, by which we can avoid loop iteration if/when values are sorted
+        //It will reduce execution time
+        public static void BubbleSort()
+        {
+            int[] arr = { 80, 11, 590, 771, 9, 770, 240, 9 };
+
+            Console.Write("\nUnsorted Array: ");
+            DisplayArray(arr);
+
+            for (int write = 0; write < arr.Length; write++)
+            {
+                bool swapped = false;
+
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        (arr[sort], arr[sort + 1]) = (arr[sort + 1], arr[sort]);
+                        swapped= true;
+                    }
+                if (swapped == false)
+                    break;
+            }
+
+            Console.Write("\nBubble Sort: ");
+            DisplayArray(arr);
+        }
+
+        //if-statement and flag variable are redundent 
+        //Removing them will reduce time and space complexity
         public static void InsertionSort()
         {
             int[] arr = new int[10] { 23, 9, 85, 12, 99, 34, 60, 15, 100, 1 };
-            int n = 10, i, j, val, flag;
-            Console.WriteLine("Insertion Sort\n\n");
-            Console.Write("Initial array is: ");
-            for (i = 0; i < n; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-            Console.Write("\n\n ");
+            int n = arr.Length, i, j, val;
+
+            Console.Write("\nUnsorted Array: ");
+            DisplayArray(arr);
 
             for (i = 1; i < n; i++)
             {
                 val = arr[i];
-                flag = 0;
-                for (j = i - 1; j >= 0 && flag != 1;)
-                {
-                    if (val < arr[j])
-                    {
-                        arr[j + 1] = arr[j];
-                        j--;
-                        arr[j + 1] = val;
-                    }
-                    else flag = 1;
-                }
+
+                for (j = i - 1; j >= 0 && val < arr[j]; j--)
+                    arr[j + 1] = arr[j];
+                arr[j + 1] = val;
             }
-            Console.Write("Sorted Array is: ");
-         for (i = 0; i < n; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
+
+            Console.Write("\nInsersion Sort: ");
+            DisplayArray(arr);
+        }
+
+        public static void DisplayArray(int[] arr)
+        {
+            foreach (int i in arr)
+                Console.Write(i + " ");
         }
     }
 }
